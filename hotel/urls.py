1518269b2370f155be86eb8e91f48968ff3342cf
Migue-C <xd.9.miguel@gmail.com>
2019-12-02 15:34:07 -0300

    Segundo commit

--- hotel/urls.py
+++ hotel/urls.py
@@ -2,15 +2,22 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
+from django.contrib import admin
+from django.urls import path
+
 
 urlpatterns = [
     path('', views.hotel_list, name='hotel_list'),
     path('hotel/acerca', views.acerca, name='Acercade'),
     path('hotel/contacto', views.contacto, name='Contacto'),
     path('hotel/inicio', views.inicio, name='Inicio'),
-    path('hotel/agregar_hotel', views.AgregarHotel, name='AgregarHotel'),
-     path('hotel/login', views.AgregarHotel, name='Login'),            
-]
+    path('hotel/agregar_hotel', views.AgregarHotel, name='AgregarHotel'),   
+    path('', views.welcome),
+    path('register', views.register),
+    path('login', views.login),
+    path('logout', views.logout),
 
+    path('admin/', admin.site.urls),        
+]
 
 
